87e6714473b349bd04e22008db0f467c88d31387
Nama Anda <email@example.com>
2025-11-26 10:32:50 +0700

    generate and migrate

--- drizzle.config.js
+++ drizzle.config.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 
 export default {
     dialect: 'postgresql',
-    schema: './db/schema.js',
+    schema: './src/db/schema.js',
     out: './drizzle',
     dbCredentials: {
         url: process.env.DATABASE_URL,
